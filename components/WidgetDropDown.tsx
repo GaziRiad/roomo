79824eff4109979501f027986ddc0fa816665f54
GaziRiad <ouhibimustapha36@gmail.com>
2024-11-16 20:37:52 +0100

    made todolist and timer appear on globalstate

--- components/WidgetDropDown.tsx
+++ components/WidgetDropDown.tsx
@@ -8,10 +8,14 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
+import { useTimer, useTodoList } from "@/store";
 import { FaRegStickyNote } from "react-icons/fa";
 import { PiNotePencilBold, PiTimerBold } from "react-icons/pi";
 
 export default function WidgetDropDown() {
+  const timer = useTimer();
+  const todoList = useTodoList();
+
   return (
     <DropdownMenu>
       <DropdownMenuTrigger className="w-fit cursor-pointer rounded-full bg-slate-950/95 px-8 py-3 text-xl font-medium uppercase">
@@ -20,14 +24,14 @@ export default function WidgetDropDown() {
       <DropdownMenuContent className="mt-2 min-w-64 border-slate-950 bg-slate-950/95 text-white">
         <DropdownMenuLabel className="py-5">ADD WIDGETS</DropdownMenuLabel>
         <DropdownMenuSeparator className="mx-auto bg-white/40" />
-        <DropdownMenuItem
-          className="py-5"
-          onClick={() => console.log("OPEN timer")}
-        >
+        <DropdownMenuItem className="py-5" onClick={() => timer.showTimer()}>
           <PiTimerBold />
           <span>Timer</span>
         </DropdownMenuItem>
-        <DropdownMenuItem className="py-5" onClick={() => console.log("OPEN")}>
+        <DropdownMenuItem
+          className="py-5"
+          onClick={() => todoList.showTodoList()}
+        >
           <PiNotePencilBold />
           <span>Todo List</span>
         </DropdownMenuItem>
