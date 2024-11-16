31c2a5f6de0588f3f3c7a16b9181848197684692
GaziRiad <ouhibimustapha36@gmail.com>
2024-11-16 21:50:13 +0100

    built bottom menu bar

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
 import DraggableContainer from "@/components/DraggableContainer";
 import LogoutButton from "@/components/LogoutButton";
 import SpaceDropDown from "@/components/SpaceDropDown";
+import { Progress } from "@/components/ui/progress";
 import WidgetDropDown from "@/components/WidgetDropDown";
 import { auth } from "@/lib/auth";
 import { redirect } from "next/navigation";
 import React from "react";
+import { FaUserAlt } from "react-icons/fa";
+import { IoSettingsOutline, IoVolumeHighOutline } from "react-icons/io5";
 
 export default async function page() {
   const session = await auth();
@@ -24,6 +27,17 @@ export default async function page() {
         <LogoutButton />
       </div>
       <DraggableContainer />
+      <div className="mx-auto flex h-16 items-center justify-center gap-10 rounded-full bg-slate-500/60 px-12">
+        <div className="flex items-center gap-3">
+          <IoVolumeHighOutline size={30} className="cursor-pointer" />
+          <Progress value={66} className="w-64" />
+        </div>
+        <span className="h-10 w-[1px] bg-white"></span>
+        <div className="flex items-center gap-5">
+          <IoSettingsOutline size={26} className="cursor-pointer" />
+          <FaUserAlt size={22} className="cursor-pointer" />
+        </div>
+      </div>
     </div>
   );
 }
